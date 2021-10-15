5a7e857fa65a116c9861104a2dd2d7f7090945a7
utzlol <40377777+utzlol@users.noreply.github.com>
2021-10-16 02:21:28 +0700

    Update chemistryrandom.py

--- chemistryrandom.py
+++ chemistryrandom.py
@@ -1,4 +1,4 @@
-import json, requests
+import json, requests, os
 
 source = requests.get("https://raw.githubusercontent.com/utzlol/chemistryrandomlist/main/elementsdatabase.json")
 source = source.json()
@@ -6,7 +6,13 @@ source = source.json()
 while True:
 	nomor = input("Nomor : ")
 	nomor = int(nomor) - 1
-	chemisname = source["elements"]
-	chemisname = chemisname[nomor]
+	chemisdet = source["elements"]
+	chemisdet = chemisdet[nomor]
 	print('\n')
-	print(chemisname["name"] +"  "+ chemisname["electron_configuration"]+"\n")
+	print(f'{chemisdet["name"]} [{chemisdet["symbol"]}] \n')
+	print(f'{chemisdet["shells"]} \n')
+	print(f'{chemisdet["electron_configuration"]} \n')
+	print(f'{chemisdet["electron_configuration_semantic"]} \n\n')
+	input('')
+	os.system('clear')
+
