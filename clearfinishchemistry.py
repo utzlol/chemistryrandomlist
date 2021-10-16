520d8f291f40196bb44a62c6dcc52bc3fa59f637
utzlol <40377777+utzlol@users.noreply.github.com>
2021-10-16 09:16:21 +0700

    Update clearfinishchemistry.py

--- clearfinishchemistry.py
+++ clearfinishchemistry.py
@@ -1,9 +1,9 @@
-file = open('editelement.json','r')
+file = open('elementsdatabase.json','r')
 fileread = file.read()
 file.close()
 
 fileread = fileread.replace('}{','},{')
-filewrite = open('editelement.json','w+')
+filewrite = open('elementsdatabase.json','w+')
 b = "{"
 c = "}"
 filewrite.write(f'{b}     "elements":    [{fileread} ] {c}')
