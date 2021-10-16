40e151575591655952a8bd711fb63af1802ae000
utzlol <40377777+utzlol@users.noreply.github.com>
2021-10-16 09:12:05 +0700

    Update clearchemistrymess.py

--- clearchemistrymess.py
+++ clearchemistrymess.py
@@ -11,7 +11,7 @@ file = open('editelement.json','a')
 
 while True:
 	urlch = url[num]
-	remove = ["appearance", "atomic_mass",'boil','category','color','density','discovered_by','melt','molar_heat','named_by','number','period','phase','source','spectral_img','summary','xpos','ypos','electron_affinity','electronegativity_pauling','ionization_energies','cpk-hex']
+	remove = ["appearance", "atomic_mass",'boil','category','density','discovered_by','melt','molar_heat','named_by','number','period','phase','source','spectral_img','summary','xpos','ypos','electron_affinity','electronegativity_pauling','ionization_energies','cpk-hex']
 	for removeorder in remove:
 		del urlch[removeorder]
 	print(urlch)
